b136ef28bd1ff32a51e4c420884e62e7740b3fe0
AntonKemvik <antonkemvik@gmail.com>
2017-03-28 16:03:37 +0200

    Första upplagan

---
+++ app/src/main/java/com/example/anton/antonsgpsacc/AcceMeter.java
@@ -0,0 +1,18 @@
+package com.example.anton.antonsgpsacc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AcceMeter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acce_meter);
+    }
+
+    protected void acessAcc (View view){
+        
+    }
+}
