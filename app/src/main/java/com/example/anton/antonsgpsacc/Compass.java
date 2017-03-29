b67a23cd64fb8ffb6367f8f064c5467bddd0d5c4
AntonKemvik <antonkemvik@gmail.com>
2017-03-29 12:50:46 +0200

    Upplaga efter petters hilfe

--- app/src/main/java/com/example/anton/antonsgpsacc/Compass.java
+++ app/src/main/java/com/example/anton/antonsgpsacc/Compass.java
@@ -2,17 +2,12 @@ package com.example.anton.antonsgpsacc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class AcceMeter extends AppCompatActivity {
+public class Compass extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acce_meter);
-    }
-
-    protected void acessAcc (View view){
-
+        setContentView(R.layout.activity_compass);
     }
 }
--- app/src/main/java/com/example/anton/antonsgpsacc/Homepage.java
+++ app/src/main/java/com/example/anton/antonsgpsacc/Homepage.java
@@ -1,7 +1,9 @@
 package com.example.anton.antonsgpsacc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    public void openCompass(View view) {
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+
+    }
+    public void openACCMeter(View view) {
+        Intent intent = new Intent(this, AcceMeter.class);
+        startActivity(intent);
+
     }
 }
